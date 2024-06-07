8ac8ba8721ac26531871722da9c325b86ba2d681
MdRaihan3 <1mdr91012@gmail.com>
2024-06-07 21:17:43 +0600

    setup aggre

--- index.js
+++ index.js
@@ -426,8 +426,8 @@ async function run() {
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error asf
-        // await client.close();
+        // Ensures that the client will close when you finish/error
+        // await client.close(); sdfe
     }
 }
 run().catch(console.dir);
