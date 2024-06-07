3e371b74227ea8161f7c26c2265b0b0cfb0e72f5
MdRaihan3 <1mdr91012@gmail.com>
2024-06-07 10:43:05 +0600

    updated req status

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
 
         const userCollection = client.db('RWorkerDB').collection('users')
         const taskCollection = client.db('RWorkerDB').collection('tasks')
+        const submissionCollection = client.db('RWorkerDB').collection('submissions')
         const paymentCollection = client.db('RWorkerDB').collection('payments')
 
         // jwt related
@@ -49,7 +50,7 @@ async function run() {
             console.log("token in verifyToken", token);
             if (!token) { return res.status(401).send({ message: 'unauthorized access...' }) }
             jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-                if (err) { return res.status(403).send({ message: 'Forbidden access' }) }
+                if (err) { return res.status(400).send({ message: 'Bad Request' }) }
                 req.user = decoded
                 next()
             })
@@ -59,7 +60,7 @@ async function run() {
             const query = { email: req?.user?.email }
             const result = await userCollection.findOne(query)
             const admin = result?.role === 'admin'
-            if (!admin) { return res.status(403).send({ message: 'forbidden access from verifyAdmin' }) }
+            if (!admin) { return res.status(403).send({ message: 'forbidden access' }) }
             next()
         }
 
@@ -67,10 +68,11 @@ async function run() {
             const query = { email: req.user?.email }
             const result = await userCollection.findOne(query)
             const taskCreator = result.role === 'taskCreator'
-            if (taskCreator) { return res.status(403).send({ message: 'forbidden access' }) }
+            if (!taskCreator) { return res.status(403).send({ message: 'forbidden access ...' }) }
             next()
         }
 
+        // user related
         app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user?.email }
@@ -129,7 +131,7 @@ async function run() {
         app.post('/tasks', async (req, res) => {
             const taskInfos = req.body;
             const filter = { email: taskInfos?.creatorEmail }
-            const taskCoin = parseFloat(taskInfos?.payableAmount)
+            const taskCoin = parseFloat(taskInfos?.payableAmount * taskInfos?.taskQuantity)
             const updateDoc = { $inc: { coin: -taskCoin } }
             const deleteResult = await userCollection.updateOne(filter, updateDoc)
             const result = await taskCollection.insertOne(taskInfos)
@@ -143,14 +145,25 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/tasks/:email', async (req, res) => {
+        app.get('/tasks/get', verifyToken, verifyAdmin, async (req, res) => {
+            const result = await taskCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/tasks/:email', verifyToken, verifyTaskCreator, async (req, res) => {
             const email = req.params?.email;
             const query = { creatorEmail: email }
             const result = await taskCollection.find(query).toArray()
             res.send(result)
         })
 
-        app.patch('/task/update/:id', async (req, res) => {
+        app.get('/task-list', verifyToken, async (req, res) => {
+            const query = { taskQuantity: { $gt: 1 } }
+            const tasks = await taskCollection.find(query).toArray()
+            res.send(tasks)
+        })
+
+        app.patch('/task/update/:id', verifyToken, verifyTaskCreator, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
             const taskData = req?.body;
@@ -159,10 +172,67 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/tasks/:id', async (req, res) => {
+        app.delete('/tasks/:id', verifyToken, async (req, res) => {
             const ids = req.params?.id;
             const query = { _id: new ObjectId(ids) }
+            const task = await taskCollection.findOne(query)
             const result = await taskCollection.deleteOne(query)
+            console.log('task', 11, task);
+            const filter = { email: task?.creatorEmail }
+            const totalCoin = parseInt(task?.taskQuantity * task?.payableAmount)
+            const updateDoc = { $inc: { coin: totalCoin } }
+            const coinUpdateResult = await userCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+        // submission related
+        app.post('/submission', async (req, res) => {
+            const submissionData = req.body;
+            const result = await submissionCollection.insertOne(submissionData)
+            const filter = { _id: new ObjectId(submissionData?.taskId) }
+            const updateDoc = { $inc: { taskQuantity: -1 } }
+            const taskQuantityResult = await taskCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+        app.get('/submission/:email', verifyToken, async (req, res) => {
+            const email = req.params?.email;
+            const query = { workerEmail: email }
+            const result = await submissionCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get('/submission/creatorEmail/:email', verifyToken, async (req, res) => {
+            const email = req.params?.email;
+            const query = { creatorEmail: email }
+            const result = await submissionCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.patch('/approve/:id', verifyToken, async (req, res) => {
+            const workerInfo = req.body
+            console.log(workerInfo)
+            const workerFilter = { email: workerInfo?.workerEmail }
+            const coin = parseInt(workerInfo?.coin)
+            const workerUpdateDoc = { $inc: { coin: coin } }
+            const coinUpdateResult = await userCollection.updateOne(workerFilter, workerUpdateDoc)
+
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: { status: 'approve' }
+            }
+            const result = await submissionCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+        app.patch('/reject/:id', verifyToken, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: { status: 'rejected' }
+            }
+            const result = await submissionCollection.updateOne(filter, updateDoc)
             res.send(result)
         })
 
@@ -180,14 +250,14 @@ async function run() {
             })
         })
 
-        app.get('/payment/get/:email', async(req, res)=>{
+        app.get('/payment/get/:email', verifyToken, verifyTaskCreator, async (req, res) => {
             const email = req.params?.email;
-            const query = {email: email}
+            const query = { email: email }
             const result = await paymentCollection.find(query).toArray()
             res.send(result)
         })
 
-        app.patch('/payment/update/:email', async (req, res) => {
+        app.patch('/payment/update/:email', verifyToken, verifyTaskCreator, async (req, res) => {
             const paymentInfo = req.body?.paymentInfo
             console.log(paymentInfo)
             const coins = parseFloat(paymentInfo?.purchasedCoin)
@@ -199,11 +269,90 @@ async function run() {
 
             const paymentResult = await paymentCollection.insertOne(paymentInfo)
 
-            res.send({coinResult, paymentResult})
+            res.send({ coinResult, paymentResult })
+        })
+
+        // state-related
+        app.get('/admin-state', verifyToken, verifyAdmin, async (req, res) => {
+            const totalUser = await userCollection.estimatedDocumentCount()
+            const revenue = await userCollection.aggregate([
+                {
+                    $group: {
+                        _id: null,
+                        price: { $sum: '$coin' }
+                    }
+                }
+            ]).toArray()
+            const totalCoin = revenue.length > 0 ? revenue[0].price : 0
+
+            const getPayment = await paymentCollection.aggregate([
+                {
+                    $group: {
+                        _id: null,
+                        payments: { $sum: '$amount' }
+                    }
+                }
+            ]).toArray()
+            let totalPayment = getPayment.length > 0 ? getPayment[0].payments : 0
+            if (totalPayment > 0) { totalPayment = totalPayment / 100 }
+
+            res.send({ totalUser, totalCoin, totalPayment })
+        })
+
+        app.get('/task-creator-state/:email', verifyToken, verifyTaskCreator, async (req, res) => {
+            const email = req.params?.email
+            const tasks = await taskCollection.aggregate([
+                {
+                    $match: { creatorEmail: email }
+                },
+                {
+                    $group: {
+                        _id: '$creatorEmail',
+                        taskQuantity: { $sum: '$taskQuantity' }
+                    }
+                }
+            ]).toArray()
+            const totalQuantity = tasks.length > 0 ? tasks[0].taskQuantity : 0
+
+            const payments = await paymentCollection.aggregate([
+                {
+                    $match: { email: email }
+                },
+                {
+                    $group: {
+                        _id: '$email',
+                        amount: { $sum: '$amount' }
+                    }
+                }
+            ]).toArray()
+            let totalPayment = payments.length > 0 ? payments[0].amount : 0
+            totalPayment = totalPayment / 100
+
+            res.send({ totalQuantity, totalPayment })
+        })
+
+        app.get('/worker-state/:email', verifyToken,  async (req, res) => {
+            const email = req.params?.email
+            const totalSubmission = await submissionCollection.estimatedDocumentCount()
+            const revenue = await submissionCollection.aggregate([
+                {
+                    $match: {workerEmail: email, status: 'approve'}
+                },
+                {
+                    $group: {
+                        _id: null,
+                        price: { $sum: '$payableAmount' }
+                    }
+                }
+            ]).toArray()
+            const totalCoin = revenue.length > 0 ? revenue[0].price : 0
+
+            res.send({ totalSubmission, totalCoin })
         })
 
 
-        // Send a ping to confirm a successful connection fasf
+
+        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
